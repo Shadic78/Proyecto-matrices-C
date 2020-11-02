79de13a063275de69f2931098966e07507b8f91f
EmmanuelCh <emmanuelisai_28@hotmail.com>
2020-11-02 17:49:38 -0600

    Fixed

--- Prueba1/Alg_ResolveSystemEquation.cpp
+++ Prueba1/Alg_ResolveSystemEquation.cpp
@@ -6,22 +6,23 @@ t_vector resolveSystemEquation(t_matrix matrix, int matrixNumRows, int matrixNum
 	t_matrix matrixSolution = copyMatrix(matrix, matrixNumRows, matrixNumCols);
 
 	//pivotPosition representa tanto la fila y columna en la que se encuentra el pivote
-	for (int pivotPosition = 0; pivotPosition < matrixNumRows; pivotPosition++)
-	{
+	for (int pivotPosition = 0; pivotPosition < matrixNumRows; pivotPosition++){
 
 		if (matrixSolution[pivotPosition][pivotPosition] != 1) {
+
 			double diagonalValue = matrixSolution[pivotPosition][pivotPosition];
 			if (diagonalValue == 0) {
 				//_STATIC_ASSERT("Division entre cero");
 				break;
 			}
 			double inverseDiagonalValue = 1 / diagonalValue;
+
 			multiplyRowBy(inverseDiagonalValue, pivotPosition, matrixSolution, matrixNumCols);
 		}
 
 		//check the others value of the colum
-		for (int rowIndex = 0; rowIndex < matrixNumRows; rowIndex++)
-		{
+		for (int rowIndex = 0; rowIndex < matrixNumRows; rowIndex++){
+
 			bool isPivot = (rowIndex == pivotPosition);
 			if (isPivot) {
 				continue;
@@ -32,12 +33,9 @@ t_vector resolveSystemEquation(t_matrix matrix, int matrixNumRows, int matrixNum
 			if (valueCell != 0) {
 				valueCell *= -1;
 
-				for (int colIndex = 0; colIndex < matrixNumCols; colIndex++)
-				{
+				for (int colIndex = 0; colIndex < matrixNumCols; colIndex++){
 				
 					double matrixPivotRowByValue = valueCell * matrixSolution[pivotPosition][colIndex];
-
-
 					double matrixCellValue = matrixSolution[rowIndex][colIndex] + matrixPivotRowByValue;
 
 					matrixSolution[rowIndex][colIndex] = matrixCellValue;
@@ -48,7 +46,6 @@ t_vector resolveSystemEquation(t_matrix matrix, int matrixNumRows, int matrixNum
 	}
 
 	t_vector solution = createVector(matrixNumRows);
-
 	for (int i = 0; i < matrixNumRows; i++){
 		solution[i] = matrixSolution[i][matrixNumCols - 1];
 	}
